5d6f6084ec37f5a38cf09358a5e7b5f3aac4cbfc
Colin Rodrigues <colin.rods@gmail.com>
2024-09-29 20:09:51 +0200

    initial commit

--- main.py
+++ main.py
@@ -21,23 +21,31 @@ def show_on_map(state):
     write_state = df[df.state.str.lower() == state]
     turtle.penup()
     turtle.goto(int(write_state.x), int(write_state.y))
-    turtle.write(f"{state.title()}", font=('Arial', 8, 'normal'))
+    turtle.write(f"{state.title()}",align="center", font=('Arial', 8, 'normal'))
     turtle.goto(0, 0)
-
 
 df = pandas.read_csv("50_states.csv")
 all_states = df.state.str.lower().to_list()
 guessed_states = []
+output_states = []
 
 while game_is_on:
     screen.update()
     user_answer = get_input().lower()
-    for state in df.state:
-        if user_answer.lower() in all_states and user_answer.lower() not in guessed_states:
-            score += 1
-            guessed_states.append(user_answer.lower())
-            show_on_map(user_answer)
+    if user_answer == "exit":
+        for states in all_states:
+            if states not in guessed_states:
+                output_states.append(states)
+        print(output_states)
+        df = pandas.DataFrame(output_states)
+        df.to_csv("Output.csv")
+
+    if user_answer.lower() in all_states and user_answer.lower() not in guessed_states:
+        score += 1
+        guessed_states.append(user_answer.lower())
+        show_on_map(user_answer)
     if len(guessed_states) == 50:
         game_is_on = False
+
 
 turtle.mainloop()
